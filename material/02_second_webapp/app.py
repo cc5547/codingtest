7f4a0e11d59ee791577a0f2ea058646cc24e88ed
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-06 19:25:40 +0900

    커밋

--- material/02_second_webapp/app.py
+++ material/02_second_webapp/app.py
@@ -4,4 +4,14 @@ import streamlit as st
 # 은행 이자 계산기
 st.write("# 은행 이자 계산기")
 
-st.img(<center><img src="https://i.imgur.com/D7uu8FN.jpg" width="300" height="200"></center>)
+# st.img(
+#   <center>
+#   <img src="https://i.imgur.com/D7uu8FN.jpg" width="300" height="200">
+#   </center>
+#   )
+
+st.img(
+    # "https://imgur.com/D7uu8FN   // <img src="https://i.imgur.com/D7uu8FN.jpg" width="300" height="200">
+    "https://blog.kakaocdn.net/dn/JvPqb/btqAiWpd8sf/Ah9IYr6lDRBb8oxSCJYQyK/img.png"
+  
+  )

39f39dc9c63cf82027d10ef7985c37b52d7c61aa
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-06 18:42:33 +0900

    test

--- material/02_second_webapp/app.py
+++ material/02_second_webapp/app.py
@@ -1,44 +1,24 @@
 # streamlit 라이브러리 호출
 # 상대경로 복사 : material/02_second_webapp/app.py
+
 import streamlit as st
-import random
+import pandas as pd
+import numpy as np
+import plotly.express as px
 
-# st.write("함수를 응용해서 페이지 만들어보기")
+st.title("데이터 시각화 예제")
 
-st.write("# 행운 뽑기")
+# 데이터 생성
+df = pd.DataFrame({
+    'x': np.random.randn(1000),
+    'y': np.random.randn(1000),
+    'category': pd.Categorical(np.random.choice(['A', 'B', 'C'], 1000))
+})
 
-# 0~9까지의 번호를 고르면
+# 그래프 그리기
+fig = px.scatter(df, x='x', y='y', color='category')
+st.plotly_chart(fig)
 
-number = st.selectbox("번호를 골라주세요", list(range(10)))
-st.write(f"내가 고른 번호 : {number}")
-
-# 상금 -> 상금을 내가 입력하게 함
-
-# prize = st.number_input(
-#   "당첨 시의 상금을 입력해주세요",
-#   min_value=1000,
-#   max_value=10000
-# )
-prize = st.slider(
-  "당첨 시의 상금을 입력해주세요",
-  1000,
-  10000,
-  step=100,
-)
-st.write(f"당첨 시의 상금 : {prize}")
-
-def my_fun():
-  # random.choice? -> 추첨 로직
-  st.balloons()
-
-# button -> 당첨입니다! / 떨어졌습니다
-pushed = st.button("당첨 알아보기", on_click=my_fun)
-
-if pushed:
-  # st.write("버튼이 눌렸습니다")
-  result = random.choice(range(10))
-  st.info(f"뽑힌 번호는... {result}번!")
-  if result == number:
-    st.success(f"축하합니다! 당첨입니다. 상금 {prize}원을 받으세요!")
-  else:
-    st.error(f"아쉽습니다! 낙첨입니다. 상금 {prize}원은 없던일로 ㅠㅠ")
+# 표 그리기
+st.subheader("데이터 표시")
+st.write(df)

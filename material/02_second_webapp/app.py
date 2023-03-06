665b08435fa937565466d19bd159b90fa2dba067
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-06 20:18:56 +0900

    커밋

--- material/02_second_webapp/app.py
+++ material/02_second_webapp/app.py
@@ -1,17 +1,18 @@
 # streamlit 라이브러리 호출
 # 상대경로 복사 : material/02_second_webapp/app.py
-import streamlit as st
-# 은행 이자 계산기
+
 # , use_column_width = True << 가운데 정렬
 # st.number_input(label, value) << 데이터 입력
 
+# 은행 이자 계산기
+import streamlit as st
 
 start_ment = "# 은행 이자 계산기"
 picture_URL = "https://i.imgur.com/D7uu8FN.jpg"
 
 with st.container():
-  st.write(start_ment)
-  st.image(picture_URL, width=300, height=200)
+    st.write(start_ment)
+    st.image(picture_URL, width=300, height=200)
 
 
 # st.write(

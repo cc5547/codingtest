31e5e6a38c261c9f10f9a87d610f573ded30630b
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-06 20:10:17 +0900

    커밋

--- material/02_second_webapp/app.py
+++ material/02_second_webapp/app.py
@@ -3,14 +3,14 @@
 import streamlit as st
 # 은행 이자 계산기
 
-# start_picture = "https://i.imgur.com/D7uu8FN.jpg"
+start_picture = "https://i.imgur.com/D7uu8FN.jpg"
 # , use_column_width = True << 가운데 정렬
 # st.number_input(label, value) << 데이터 입력
 
 start_ment = "# 은행 이자 계산기"
 
 st.write(start_ment)
-st.image("https://i.imgur.com/D7uu8FN.jpg", width=300, height=200) 
+st.image(start_picture, width=300, height=200)
 
 
 

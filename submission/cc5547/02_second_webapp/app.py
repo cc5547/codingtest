11140d01f4e1fe58ee2b82c7d44f6ae884e743b2
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-07 19:30:12 +0900

    push

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -10,11 +10,19 @@ with col2 :
   # column 2 에 담을 내용
   st.title('here is column2')
   st.checkbox('this is checkbox1 in col2 ')
+with col3 :
+  # column 3 에 담을 내용
+  st.title('here is column3')
+  st.checkbox('this is checkbox1 in col3 ')
+with col4 :
+  # column 4 에 담을 내용
+  st.title('here is column4')
+  st.checkbox('this is checkbox1 in col4 ')
 
 
 # with 구문 말고 다르게 사용 가능 
-col1.subheader(' i am column1  subheader !! ')
-col2.checkbox('this is checkbox2 in col2 ') 
+# col1.subheader(' i am column1  subheader !! ')
+# col2.checkbox('this is checkbox2 in col2 ') 
 #=>위에 with col2: 안의 내용과 같은 기능을합니다
 
 

3d2a25faab7ba86fb9a20b001ceedbb6f52f4a73
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-10 11:51:32 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.py
+++ 프로그래머스/lv0/120891. 369게임/369게임.py
@@ -1,13 +1,2 @@
 def solution(order):
-    answer = 0
-    order_list = list(str(order))
-
-    for i in range(len(order_list)):
-        if "3" == order_list[i] : 
-            answer += 1
-        elif "6" == order_list[i]:
-            answer += 1
-        elif "9" == order_list[i]:
-            answer += 1
-        else :pass
-    return answer
+    return str(order).count('3') + str(order).count('6') + str(order).count('9')
